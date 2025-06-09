a2cd59be1cebf480214337340087e668e56a9d24
matiar <110628257+Matiar15@users.noreply.github.com>
2025-06-09 21:56:47 +0200

    correct code + new results

--- example/efficient_net_b0_finetuned.py
+++ example/efficient_net_b0_finetuned.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
-from torchvision.models import efficientnet_b0
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 
 
 class EfficientNetB0FineTuned(nn.Module):
     def __init__(self):
         super(EfficientNetB0FineTuned, self).__init__()
 
-        self.model = efficientnet_b0(pretrained=True)
+        self.model = efficientnet_b0(weights=EfficientNet_B0_Weights.DEFAULT)
         for param in self.model.features.parameters():
             param.requires_grad = False
         self.model.classifier[1] = nn.Linear(self.model.classifier[1].in_features, 10)
